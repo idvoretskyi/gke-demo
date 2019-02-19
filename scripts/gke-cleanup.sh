fa3a39b70724910777bb5980c04cc6d0fe7bbe0b
Ihor Dvoretskyi <ihor@linux.com>
2019-02-19 17:31:49 +0000

    Major updates
    Signed-off-by: Ihor Dvoretskyi <ihor@linux.com>

--- scripts/gke-cleanup.sh
+++ scripts/gke-cleanup.sh
@@ -3,12 +3,11 @@
 # Setting environment variables
 
 export CLUSTER_NAME=idv-gke-01
-export CLUSTER_ZONE=us-central1-c
-export PROJECT_ID=idv-dev-project-01
+export CLUSTER_ZONE=us-east1-c
+export PROJECT_ID=idv-dev-0
 
 # Set the default project in gcloud shell
 gcloud config set project $PROJECT_ID
-
 
 # To delete the cluster, enter the following command (removing '#' before):
 
--- scripts/gke-install.sh
+++ scripts/gke-install.sh
@@ -3,32 +3,33 @@
 # Setting environment variables
 
 export CLUSTER_NAME=idv-gke-01
-export CLUSTER_ZONE=us-central1-c
-export PROJECT_ID=idv-dev-project-01
+export CLUSTER_ZONE=us-east1-c
+export PROJECT_ID=idv-dev-0
 
 # Set the default project in gcloud shell
 gcloud config set project $PROJECT_ID
 
 # Enabling the necessary APIs
 gcloud services enable \
-  cloudapis.googleapis.com \
-  container.googleapis.com \
-  containerregistry.googleapis.com
-  
+	cloudapis.googleapis.com \
+	container.googleapis.com \
+	containerregistry.googleapis.com
+
 # Create a GKE cluster
 gcloud container clusters create $CLUSTER_NAME \
-  --zone=$CLUSTER_ZONE \
-  --cluster-version=latest \
-  --machine-type=n1-standard-4 \
-  --enable-autoscaling --min-nodes=1 --max-nodes=10 \
-  --enable-autorepair \
-  --scopes=service-control,service-management,compute-rw,storage-ro,cloud-platform,logging-write,monitoring-write,pubsub,datastore \
-  --num-nodes=3
-  
+	--zone=$CLUSTER_ZONE \
+	--cluster-version=latest \
+	--machine-type=f1-micro \
+	--enable-autoscaling --min-nodes=1 --max-nodes=10 \
+	--enable-autorepair \
+	--preemptible \
+	--scopes=service-control,service-management,compute-rw,storage-ro,cloud-platform,logging-write,monitoring-write,pubsub,datastore \
+	--num-nodes=3
+
 # To authenticate for the cluster, run the following command:
-gcloud container clusters get-credentials $CLUSTER_NAME
+gcloud container clusters get-credentials $CLUSTER_NAME --zone=$CLUSTER_ZONE
 
 # Grant cluster-admin permissions to the current user:
 kubectl create clusterrolebinding cluster-admin-binding \
-  --clusterrole=cluster-admin \
-  --user=$(gcloud config get-value core/account)
+	--clusterrole=cluster-admin \
+	--user=$(gcloud config get-value core/account)
